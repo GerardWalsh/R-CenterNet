3a80aa00cbc2ec0bd9596a50d6a036d12553a9ad
GerardWalsh <gerar9207@gmail.com>
2021-06-01 09:49:13 +0200

    Add args for training

--- backbone/resnet.py
+++ backbone/resnet.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan  5 14:06:48 2020
-
-@author: Lim
-"""
 import torch.nn as nn
+
+# sys.path.append(r"../backbone")
+# from ..predict import ctdet_decode
 
 
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+    return nn.Conv2d(
+        in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+    )
 
 
 class BasicBlock(nn.Module):
@@ -52,11 +50,13 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1,
-                               bias=False)
+        self.conv3 = nn.Conv2d(
+            planes, planes * self.expansion, kernel_size=1, bias=False
+        )
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -87,36 +87,44 @@ class Bottleneck(nn.Module):
 
 class Creat_ResNet(nn.Module):
     def __init__(self, block, layers, heads, head_conv, plot, **kwargs):
-        
+
         self.heads = heads
         self.plot = plot
-        
+
         self.inplanes = 64
         super(Creat_ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.sigmoid = nn.Sigmoid()
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
 
         # used for deconv layers
-        self.deconv_layers = self._make_deconv_layer(3,[256, 256, 256],[4, 4, 4])
+        self.deconv_layers = self._make_deconv_layer(3, [256, 256, 256], [4, 4, 4])
         # self.final_layer = []
 
         for head in sorted(self.heads):
             num_output = self.heads[head]
             if head_conv > 0:
                 fc = nn.Sequential(
-                        nn.Conv2d(256, head_conv, kernel_size=3, padding=1, bias=True),
-                        nn.ReLU(inplace=True),
-                        nn.Conv2d(head_conv, num_output, kernel_size=1, stride=1, padding=0)
-                        )
+                    nn.Conv2d(256, head_conv, kernel_size=3, padding=1, bias=True),
+                    nn.ReLU(inplace=True),
+                    nn.Conv2d(
+                        head_conv, num_output, kernel_size=1, stride=1, padding=0
+                    ),
+                )
             else:
-                fc = nn.Conv2d(in_channels=256,out_channels=num_output, kernel_size=1,stride=1,padding=0)
+                fc = nn.Conv2d(
+                    in_channels=256,
+                    out_channels=num_output,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                )
             self.__setattr__(head, fc)
 
         # self.final_layer = nn.ModuleList(self.final_layer)
@@ -125,8 +133,13 @@ class Creat_ResNet(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
@@ -137,8 +150,6 @@ class Creat_ResNet(nn.Module):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
-
-
 
     def _get_deconv_cfg(self, deconv_kernel, index):
         if deconv_kernel == 4:
@@ -153,24 +164,29 @@ class Creat_ResNet(nn.Module):
         return deconv_kernel, padding, output_padding
 
     def _make_deconv_layer(self, num_layers, num_filters, num_kernels):
-        assert num_layers == len(num_filters), \
-            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
-        assert num_layers == len(num_kernels), \
-            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
+        assert num_layers == len(
+            num_filters
+        ), "ERROR: num_deconv_layers is different len(num_deconv_filters)"
+        assert num_layers == len(
+            num_kernels
+        ), "ERROR: num_deconv_layers is different len(num_deconv_filters)"
 
         layers = []
         for i in range(num_layers):
-            kernel, padding, output_padding = \
-                self._get_deconv_cfg(num_kernels[i], i)
+            kernel, padding, output_padding = self._get_deconv_cfg(num_kernels[i], i)
 
             planes = num_filters[i]
-            layers.append(nn.ConvTranspose2d(in_channels=self.inplanes,
-                                             out_channels=planes,
-                                             kernel_size=kernel,
-                                             stride=2,
-                                             padding=padding,
-                                             output_padding=output_padding,
-                                             bias=False))
+            layers.append(
+                nn.ConvTranspose2d(
+                    in_channels=self.inplanes,
+                    out_channels=planes,
+                    kernel_size=kernel,
+                    stride=2,
+                    padding=padding,
+                    output_padding=output_padding,
+                    bias=False,
+                )
+            )
             layers.append(nn.BatchNorm2d(planes))
             layers.append(nn.ReLU(inplace=True))
             self.inplanes = planes
@@ -190,21 +206,37 @@ class Creat_ResNet(nn.Module):
 
         x = self.deconv_layers(x)
         ret = {}
-        res = [] 
+        res = []
         for head in self.heads:
             ret[head] = self.__getattr__(head)(x)
-            res.append(self.__getattr__(head)(x))  
-        return res if self.plot else ret
+            res.append(self.__getattr__(head)(x))
+        # ret["heat"] = self.sigmoid(ret["hm"])
+        # detections = ctdet_decode(**ret)
+        # import ipdb
 
-resnet_spec = {18: (BasicBlock, [2, 2, 2, 2]),
-               34: (BasicBlock, [3, 4, 6, 3]),
-               50: (Bottleneck, [3, 4, 6, 3]),
-               101: (Bottleneck, [3, 4, 23, 3]),
-               152: (Bottleneck, [3, 8, 36, 3])}
+        # ipdb.set_trace()
+        return ret
 
-def ResNet(layer_num, heads = {'hm': 1, 'wh': 2, 'ang':1, 'reg': 2}, head_conv=256, plot=False):
-    assert layer_num in [18,34,50,101,152], \
-            'ERROR: layer_num must be in [18,34,50,101,152]'
+
+resnet_spec = {
+    18: (BasicBlock, [2, 2, 2, 2]),
+    34: (BasicBlock, [3, 4, 6, 3]),
+    50: (Bottleneck, [3, 4, 6, 3]),
+    101: (Bottleneck, [3, 4, 23, 3]),
+    152: (Bottleneck, [3, 8, 36, 3]),
+}
+
+
+def ResNet(
+    layer_num, heads={"hm": 1, "wh": 2, "ang": 1, "reg": 2}, head_conv=256, plot=False
+):
+    assert layer_num in [
+        18,
+        34,
+        50,
+        101,
+        152,
+    ], "ERROR: layer_num must be in [18,34,50,101,152]"
     block_class, layers = resnet_spec[layer_num]
     model = Creat_ResNet(block_class, layers, heads, head_conv=head_conv, plot=plot)
     return model
--- train.py
+++ train.py
@@ -1,129 +1,181 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan  5 13:57:15 2020
-
-@author: Lim
-"""
+import argparse
 import time
 import os
 import sys
+
 import torch
 import numpy as np
-from Loss import CtdetLoss
+
 from torch.utils.data import DataLoader
-from torchvision import transforms 
+from torchvision import transforms
 from opencv_transforms import transforms
 from torchsummary import summary
-from opencv_transforms import transforms
 
 from dataset import ctDataset
+from Loss import CtdetLoss
 
-sys.path.append(r'./backbone')
-# from resnet import ResNet  
-from resnet_dcn import ResNet
-#from dlanet import DlaNet
-from dlanet_dcn import DlaNet
-import predict
+sys.path.append(r"./backbone")
+from resnet import ResNet
 
-os.environ["CUDA_VISIBLE_DEVICES"] = '0' 
+# from resnet_dcn import ResNet
+# from dlanet import DlaNet
+# from dlanet_dcn import DlaNet
+# import predict
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="resnet_18",
+        help="Name of the model arch.",
+    )
+    parser.add_argument("--lr", type=float, default=1e-3, help="Base learning rate.")
+    parser.add_argument("--input-size", type=int, default=224, help="Image input size.")
+    parser.add_argument(
+        "--center",
+        type=bool,
+        default=False,
+        help="Whether to center the data by the mean and std dev.",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=10,
+        help="Whether data has been normalized.",
+    )
+    return parser.parse_args()
+
+
+args = parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 use_gpu = torch.cuda.is_available()
-# model = ResNet(34)
-model = DlaNet(34)
+model = ResNet(int(args.model.split("_")[-1]))
+# model = DlaNet(34)
 
-# print('cuda', torch.cuda.current_device(), torch.cuda.device_count())
 
-loss_weight={'hm_weight':1, 'wh_weight':0.1, 'ang_weight':0.5, 'reg_weight':0.1}
+loss_weight = {"hm_weight": 1, "wh_weight": 0.1, "ang_weight": 0.5, "reg_weight": 0.1}
 criterion = CtdetLoss(loss_weight)
 
 device = torch.device("cuda")
 if use_gpu:
     model.cuda()
-    
+
 # summary(model, input_size=(3, 512, 512))
 
 model.train()
 
-learning_rate = 1e-3
-num_epochs = 15
+learning_rate = float(args.lr)
+num_epochs = int(args.epochs)
 
 # different learning rate
-params=[]
+params = []
 params_dict = dict(model.named_parameters())
-for key,value in params_dict.items():
-    params += [{'params':[value],'lr':learning_rate}]
+for key, value in params_dict.items():
+    params += [{"params": [value], "lr": learning_rate}]
 
-#optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
-optimizer = torch.optim.Adam(params, lr=learning_rate, weight_decay=1e-4)
+# optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
+# optimizer = torch.optim.Adam(params, lr=learning_rate, weight_decay=1e-4)
+optimizer = torch.optim.RMSprop(params)
 
-transform = transforms.Compose([
-    transforms.RandomGrayscale(),
-    # # transforms.ToTensor(),
-    # torch.from_numpy,
-    # transforms.ToPILImage(),
-    # transforms.ColorJitter(hue=.5, saturation=.5)
-    # transforms.ToTensor()
-])
+transform = transforms.Compose(
+    [
+        transforms.RandomGrayscale(),
+        # # transforms.ToTensor(),
+        # torch.from_numpy,
+        # transforms.ToPILImage(),
+        # transforms.ColorJitter(hue=.5, saturation=.5)
+        # transforms.ToTensor()
+    ]
+)
 
-train_dataset = ctDataset(split='train', transform=transform)
+train_dataset = ctDataset(
+    split="train",
+    transform=transform,
+    input_size=int(args.input_size),
+    center=bool(args.center),
+)
 # train_dataset = ctDataset(split='train')
 train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=0)
 
-test_dataset = ctDataset(split='val')
+test_dataset = ctDataset(split="val")
 test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=0)
-print('the dataset has %d images' % (len(train_dataset)))
+print("the dataset has %d images" % (len(train_dataset)))
 
 
 num_iter = 0
 
-best_test_loss = np.inf 
+best_test_loss = np.inf
 
 start = time.perf_counter()
 
 for epoch in range(num_epochs):
     model.train()
     if epoch == 90:
-        learning_rate= learning_rate * 0.1 
+        learning_rate = learning_rate * 0.1
     if epoch == 120:
-        learning_rate= learning_rate * (0.1 ** 2)
+        learning_rate = learning_rate * (0.1 ** 2)
     for param_group in optimizer.param_groups:
-        param_group['lr'] = learning_rate
-     
-    total_loss = 0.
-    
+        param_group["lr"] = learning_rate
+
+    total_loss = 0.0
+
     for i, sample in enumerate(train_loader):
 
         for k in sample:
             sample[k] = sample[k].to(device=device, non_blocking=True)
 
-        pred = model(sample['input'])
-        loss = criterion(pred, sample)    
+        pred = model(sample["input"])
+        loss = criterion(pred, sample)
         total_loss += loss.item()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        if (i+1) % 30 == 0:
-            print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f, average_loss: %.4f' 
-            %(epoch+1, num_epochs, i+1, len(train_loader), loss.data, total_loss / (i+1)))
+        if (i + 1) % 30 == 0:
+            print(
+                "Epoch [%d/%d], Iter [%d/%d] Loss: %.4f, average_loss: %.4f"
+                % (
+                    epoch + 1,
+                    num_epochs,
+                    i + 1,
+                    len(train_loader),
+                    loss.data,
+                    total_loss / (i + 1),
+                )
+            )
             num_iter += 1
-            
-    #validation
+
+    # validation
     validation_loss = 0.0
     model.eval()
     for i, sample in enumerate(test_loader):
         if use_gpu:
             for k in sample:
                 sample[k] = sample[k].to(device=device, non_blocking=True)
-        
-        pred = model(sample['input'])
-        loss = criterion(pred, sample)   
+
+        pred = model(sample["input"])
+        pred["hm"] = pred["hm"].sigmoid_()
+        loss = criterion(pred, sample)
         validation_loss += loss.item()
     validation_loss /= len(test_loader)
-    
-    
+
     # if best_test_loss > validation_loss:
     #     best_test_loss = validation_loss
     #     print('Got best test loss %.5f' % best_test_loss)
     #     torch.save(model.state_dict(),'best.pth')
-    torch.save(model.state_dict(),'last.pth')
+    if args.center:
+        centered = "centered"
+    else:
+        centered = ""
+    torch.save(
+        model.state_dict(),
+        f"last_{args.model}_{args.input_size}_epochs:{args.epochs}_lr:{args.lr}_RMSprop_{centered}.pth",
+    )
 
-print('{} epochs with {} frames in {} seconds'.format(num_epochs, len(train_dataset), time.perf_counter() - start))
+print(
+    "{} epochs with {} frames in {} seconds".format(
+        num_epochs, len(train_dataset), time.perf_counter() - start
+    )
+)
