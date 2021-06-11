957338f4c0881c9dd15a0ca29c4c81796272de35
Gerard Walsh <gerard9207@gmail.com>
2021-06-11 17:09:12 +0200

    Train config (#2)
    Training from config file
    
    Co-authored-by: GerardWalsh <gerar9207@gmail.com>

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import argparse
+from datetime import datetime
+import pathlib
 import time
 import os
 import sys
@@ -9,58 +11,30 @@ import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from opencv_transforms import transforms
-from torchsummary import summary
 
 from dataset import ctDataset
 from Loss import CtdetLoss
+from utils.yaml import read_yaml, write_yaml
 
 sys.path.append(r"./backbone")
 from resnet import ResNet
-
-# from resnet_dcn import ResNet
-# from dlanet import DlaNet
-# from dlanet_dcn import DlaNet
-# import predict
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model",
+        "--training-config",
         type=str,
-        default="resnet_18",
+        required=True,
         help="Name of the model arch.",
-    )
-    parser.add_argument("--lr", type=float, default=1e-3, help="Base learning rate.")
-    parser.add_argument("--input-size", type=int, default=224, help="Image input size.")
-    parser.add_argument(
-        "--center",
-        type=bool,
-        default=False,
-        help="Whether to center the data by the mean and std dev.",
-    )
-    parser.add_argument(
-        "--epochs",
-        type=int,
-        default=10,
-        help="Whether data has been normalized.",
-    )
-    parser.add_argument(
-        "--batch-size",
-        type=int,
-        default=4,
-        help="Whether data has been normalized.",
-    )
-    parser.add_argument(
-        "--optimiser",
-        type=str,
-        default="SGD",
-        help="Which optimiser to use.",
     )
     return parser.parse_args()
 
 
 args = parse_args()
+config = read_yaml(args.training_config)
+date_stamp = datetime.now().strftime("%j:%H:%M")
+
 
 opts = {
     "RMSprop": torch.optim.RMSprop,
@@ -70,8 +44,7 @@ opts = {
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 use_gpu = torch.cuda.is_available()
-model = ResNet(int(args.model.split("_")[-1]))
-# model = DlaNet(34)
+model = ResNet(int(config["model"].split("_")[-1]))
 
 
 loss_weight = {"hm_weight": 1, "wh_weight": 0.1, "ang_weight": 0.5, "reg_weight": 0.1}
@@ -81,12 +54,10 @@ device = torch.device("cuda")
 if use_gpu:
     model.cuda()
 
-# summary(model, input_size=(3, 512, 512))
-
 model.train()
 
-learning_rate = float(args.lr)
-num_epochs = int(args.epochs)
+learning_rate = float(config["learning_rate"])
+num_epochs = int(config["epochs"])
 
 # different learning rate
 params = []
@@ -94,10 +65,8 @@ params_dict = dict(model.named_parameters())
 for key, value in params_dict.items():
     params += [{"params": [value], "lr": learning_rate}]
 
-# optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0.9, weight_decay=5e-4)
-# optimizer = torch.optim.Adam(params, lr=learning_rate, weight_decay=1e-4)
-print(f"[INFO]: Using {str(args.optimiser)} optimiser.")
-optimizer = opts[str(args.optimiser)](params, lr=args.lr)
+print(f'[INFO]: Using {str(config["optimizer"])} optimiser.')
+optimizer = opts[str(config["optimizer"])](params, lr=learning_rate)
 
 transform = transforms.Compose(
     [
@@ -110,15 +79,20 @@ transform = transforms.Compose(
     ]
 )
 
+if config["center"]:
+    centered = "centered"
+else:
+    centered = ""
+
 train_dataset = ctDataset(
     split="train",
     transform=transform,
-    input_size=int(args.input_size),
-    center=bool(args.center),
+    input_size=int(config["input_size"]),
+    center=bool(config["center"]),
 )
 # train_dataset = ctDataset(split='train')
 train_loader = DataLoader(
-    train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0
+    train_dataset, batch_size=config["batch_size"], shuffle=True, num_workers=0
 )
 
 test_dataset = ctDataset(split="val")
@@ -127,10 +101,14 @@ print("[INFO]: The dataset has %d images" % (len(train_dataset)))
 
 
 num_iter = 0
-
 best_test_loss = np.inf
-
 start = time.perf_counter()
+
+# Create directory for training run
+training_directory_name = pathlib.Path(
+    f'{date_stamp}_{config["model"]}_{config["input_size"]}_epochs:{config["epochs"]}_lr:{config["learning_rate"]}_{config["optimiser"]}_{centered}_batch_size:{config["batch_size"]}'
+)
+training_directory_name.mkdir(parents=True, exist_ok=True)
 
 for epoch in range(num_epochs):
     model.train()
@@ -186,13 +164,10 @@ for epoch in range(num_epochs):
     #     best_test_loss = validation_loss
     #     print('Got best test loss %.5f' % best_test_loss)
     #     torch.save(model.state_dict(),'best.pth')
-    if args.center:
-        centered = "centered"
-    else:
-        centered = ""
+
     torch.save(
         model.state_dict(),
-        f"last_{args.model}_{args.input_size}_epochs:{args.epochs}_lr:{args.lr}_{args.optimiser}_{centered}_batch_size:{args.batch_size}.pth",
+        training_directory_name / "last.pth",
     )
 
 print(
@@ -200,3 +175,7 @@ print(
         num_epochs, len(train_dataset), time.perf_counter() - start
     )
 )
+
+print("Saving config")
+config["datestamp"] = date_stamp
+write_yaml(training_directory_name / "trained_config.yml", config)
